26f08a3034590d8f8ee99b4550cf4dc2aa67ab9a
Tejesh <tejeshkinariwala@gmail.com>
2024-12-02 23:22:21 +0800

    Fix Prisma for Edge runtime

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from '.prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+const prismaGlobal = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  log: ['query', 'error', 'warn'],
-})
+export const prisma =
+  prismaGlobal.prisma ||
+  new PrismaClient({
+    log: ['query', 'error', 'warn'],
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') prismaGlobal.prisma = prisma
 
 export type TransactionClient = Omit<
   PrismaClient,
