680b2b7b698c95b2881ced68e53eee6c0a03cc41
Tejesh <tejeshkinariwala@gmail.com>
2024-12-03 13:39:41 +0800

    Fix Bet type and build script

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -2,11 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 const prismaGlobal = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  prismaGlobal.prisma ||
-  new PrismaClient({
-    log: ['query', 'error', 'warn'],
-  })
+export const prisma = prismaGlobal.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') prismaGlobal.prisma = prisma
 
